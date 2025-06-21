710bd4c3d1c4de8d356eb9a25370e3b40dd7c981
Samim Junayet Istiaq <junayet03@gmail.com>
2025-06-22 02:03:25 +0600

    add

--- Code/myapp/src/app/app-routing-module.ts
+++ Code/myapp/src/app/app-routing-module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home';
-import { Allemployee } from './allemployee/allemployee';
 
 const routes: Routes = [
-  {path:'', component:Home },
-  {path:'allemployee', component: Allemployee }
+  {path:'home',component:Home}
 ];
 
 @NgModule({
