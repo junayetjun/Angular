c207da815d04be7f23fd1e9955b8cd86ff46c2fc
Samim Junayet Istiaq <junayet03@gmail.com>
2025-07-19 01:46:40 +0600

    add

--- Code/b-gurdian/src/app/auth/userprofile/userprofile.ts
+++ Code/b-gurdian/src/app/auth/userprofile/userprofile.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsermodelModule } from '../../model/usermodel-module';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../service/auth-service';
+import { Router } from '@angular/router';
+import { UserService } from '../../service/user-service';
+import { cpSync } from 'fs';
 
 @Component({
   selector: 'app-userprofile',
@@ -6,6 +12,43 @@ import { Component } from '@angular/core';
   templateUrl: './userprofile.html',
   styleUrl: './userprofile.css'
 })
-export class Userprofile {
+export class Userprofile implements OnInit {
+
+
+  user: UsermodelModule | null = null;
+  private subscription: Subscription = new Subscription();
+
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+
+  ngOnInit(): void {
+    //this.loadUserProfile();
+  }
+
+
+  // loadUserProfile(): void {
+  //   const sub = this.userService.getUserProfile().subscribe({
+  //     next: (res) => {
+  //       console.log(res);
+  //       if (res) {
+  //         this.user = res;
+  //       }
+  //     },
+  //     error: (err) => {
+  //       console.log(err);
+  //     }
+  //   });
+  //   this.subscription.add(sub);
+  // }
+
+
+  // ngOnDistroy(): void {
+  //   this.subscription.unsubscribe();
+  // }
 
 }
